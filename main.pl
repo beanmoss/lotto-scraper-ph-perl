4d284c620a98ae955e70d1cd76b7d61efed090f3
rluna <r.luna@arcanys.com>
2015-06-30 01:06:19 +0800

    refactored to be strict compliant

--- main.pl
+++ main.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-# use strict;
+use strict;
 use warnings;
 use LWP::Simple;
 use lib "HTML";
@@ -9,40 +9,47 @@ use Config;
 use threads;
 use FileHandle;
 use LWP::UserAgent;
-
-my $ua = new LWP::UserAgent;
+use LWP::ConnCache;
+require HTTP::Request;
     
 $Config{usethreads} or die "Recompile Perl with threads to run this program.";
 
 #Scrapes the content of the webpage of the given url.
 sub scrape() 
 {
-	$url = $_[0];
-	$file = $_[1];
+	my $url = $_[0];
+	my $file = $_[1];
+	my $te = $_[2];
+	my $ua = $_[3];
 
-	# $data =  get($url);
-	$ua->default_header('Accept-Encoding' => scalar HTTP::Message::decodable());
-	$data = $ua->get($url)->decoded_content;
-	&writeToFile($file, $data);
+	$ua->default_header('Accept-Encoding' => 'gzip');
+	my $data = $ua->get($url)->decoded_content;
+
+	writeToFile($file, $data, $te);
 }
 
 #Writes data to the file in csv format (semicolon separator since money figures are involved)
 sub writeToFile()
 {
-	$file = $_[0];
-	$data = $_[1];
-	$stream = '';
-	$te = HTML::TableExtract->new( attribs => { border => 1 } );
+	my $file = $_[0];
+	my $data = $_[1];
+	my $te = $_[2];
+	my $stream = '';
+	# $te = HTML::TableExtract->new();
 	$te->parse($data);
-	foreach $ts ($te->tables) {
-	  foreach $row ($ts->rows) {
-		$values = join(';', @$row);
+	foreach my $ts ($te->tables) {
+	  foreach my $row ($ts->rows) {
+		my $values = join(';', @$row);
 		$stream = "${stream}${values}\n";
 	  }
 	}
 	print $file $stream;
 	close $file;
 }
+
+
+
+my $ua = new LWP::UserAgent;
 
 #Stores the result
 my $resultDir = 'results/';
@@ -59,15 +66,21 @@ my $i = 0;
 
 my @files;
 
+my $te = HTML::TableExtract->new();
+
+my @fh;
+
 #Here we go!
-foreach $type (@drawTypes){
-	$fh[$i] = FileHandle->new("> ${resultDir}${type}.csv");
-	$threads[$i] = threads->create(\&scrape, "${url}${type}.asp", $fh[$i]);
+foreach my $type (@drawTypes){
+	$fh[$i] = FileHandle->new("> ${resultDir}${type}${i}.csv");
+	$threads[$i] = threads->create(\&scrape, "${url}${type}.asp", $fh[$i], $te, $ua);
 	$i++;
 }
-foreach $thread (@threads)
+
+foreach my $thread (@threads)
 {
 	$thread->join();
 }
+
 print "Complete...\n";
 exit 0;
